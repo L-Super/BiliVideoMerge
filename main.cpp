0d7bfc58d7ee03453ed8fbe99828840af3cb31c6
unknown <Listening-Work>
2022-04-24 18:11:43 +0800

    重建工程

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <QApplication>
-#include <QPushButton>
-#include "bmergewidget.h"
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    BMergeWidget b;
-    b.show();
+
     return QApplication::exec();
 }
